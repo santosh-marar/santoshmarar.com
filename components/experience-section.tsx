b91d3fe40f2d65a3134cf54a4f4d8b410891fa96
santosh-marar <st.juniordeveloper@gmail.com>
2026-06-06 00:02:55 +0545

    refactor: add target on link so it open into next tab

--- components/experience-section.tsx
+++ components/experience-section.tsx
@@ -30,9 +30,6 @@ const ExperienceSection = () => {
         </div>
 
         <Accordion
-          type="single"
-          collapsible
-          defaultValue="item-1"
           className="border-none hover:bg-secondary mt-1"
         >
           <AccordionItem value="item-1">
@@ -48,9 +45,9 @@ const ExperienceSection = () => {
                         viewBox="0 0 24 24"
                         fill="none"
                         stroke="currentColor"
-                        stroke-width="2"
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
+                        strokeWidth="2"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
                         className="lucide lucide-code-xml"
                         aria-hidden="true"
                       >
--- components/soical-section.tsx
+++ components/soical-section.tsx
@@ -1,31 +1,53 @@
 import Link from "next/link";
 
 const SocialSection = () => {
-  return(
-  <div className="pt-2 font-medium text-muted-foreground px-2">
-    <p>
-      {" "}
-      You can find me on{" "}
-      <span className="font-semibold border-b">
-        <Link href="https://x.com/santosh_marar">X</Link>
-      </span>
-      ,{" "}
-      <span className="font-semibold border-b">
-        <Link href="https://github.com/santosh-marar">GitHub</Link>
-      </span>
-      ,{" "}
-      <span className="font-semibold border-b">
-        <Link href="https://www.linkedin.com/in/santosh-kumar-marar-98536319b/">
-          LinkedIn
-        </Link>
-      </span>
-      , or schedule a meetup on{" "}
-      <span className="text-primary font-semibold border-b border-dotted">
-        <Link href="https://cal.com/santosh-marar/15-min-meeting">Cal.com</Link>
-      </span>
-    </p>
-  </div>
-)
+  return (
+    <div className="pt-2 font-medium text-muted-foreground px-2">
+      <p>
+        {" "}
+        You can find me on{" "}
+        <span className="font-semibold border-b">
+          <Link
+            href="https://x.com/santosh_marar"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            X
+          </Link>
+        </span>
+        ,{" "}
+        <span className="font-semibold border-b">
+          <Link
+            href="https://github.com/santosh-marar"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            GitHub
+          </Link>
+        </span>
+        ,{" "}
+        <span className="font-semibold border-b">
+          <Link
+            href="https://www.linkedin.com/in/santosh-kumar-marar-98536319b/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            LinkedIn
+          </Link>
+        </span>
+        , or schedule a meetup on{" "}
+        <span className="text-primary font-semibold border-b border-dotted">
+          <Link
+            href="https://cal.com/santosh-marar/15-min-meeting"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Cal.com
+          </Link>
+        </span>
+      </p>
+    </div>
+  );
 };
 
 export default SocialSection;
