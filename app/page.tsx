7189cf34056dc9f4fc32ddb0cd90e27826768a4c
santosh-marar <st.juniordeveloper@gmail.com>
2026-07-02 08:15:37 +0545

    fix: position of footer

--- app/page.tsx
+++ app/page.tsx
@@ -6,15 +6,31 @@ import BlogSection from "@/components/blog-section";
 import FooterSection from "@/components/footer-section";
 // import Navbar from "@/components/navbar-section";
 
+// export default function Page() {
+//   return (
+//     <main className="relative max-w-2xl pt-12 mx-auto">
+//       {/*<Navbar />*/}
+//       <HeroSection />
+//       <SocialSection />
+//       <ExperienceSection />
+//       <ProjectSection />
+//       <BlogSection />
+//       <FooterSection />
+//     </main>
+//   );
+// }
+
+
 export default function Page() {
   return (
-    <main className="max-w-2xl pt-12 mx-auto">
-      {/*<Navbar />*/}
-      <HeroSection />
-      <SocialSection />
-      <ExperienceSection />
-      <ProjectSection />
-      <BlogSection />
+    <main className="grid min-h-screen max-w-2xl grid-rows-[1fr_auto] mx-auto pt-12">
+      <div>
+        <HeroSection />
+        <SocialSection />
+        <ExperienceSection />
+        <ProjectSection />
+        <BlogSection />
+      </div>
       <FooterSection />
     </main>
   );
--- components/footer-section.tsx
+++ components/footer-section.tsx
@@ -1,9 +1,18 @@
 const FooterSection = () => {
   return (
-    <footer className="flex flex-col gap-2 px-2 py-4 font-medium text-muted-foreground md:flex-row md:items-center md:justify-between">
-      <p className="">Designed & Developed by <a href="https://santoshmarar.com" className="hover:border-b font-semibold">Santosh Marar</a></p>
-      {/*<p className="">The source code is available on <a href="https://github.com/santosh-marar/santoshmarar.com">GitHub</a>.</p>*/}
-      <p className="">Code available on <a href="https://github.com/santosh-marar/santoshmarar.com" className="hover:border-b font-semibold">GitHub</a>.</p>
+    <footer className="mt-2 flex w-full flex-col items-center justify-between gap-2 px-2 py-4 font-medium text-muted-foreground md:flex-row md:items-center md:justify-between">
+      <p>
+        Designed & Developed by{" "}
+        <a href="https://santoshmarar.com" className="hover:border-b font-semibold">
+          Santosh Marar
+        </a>
+      </p>
+      <p>
+        Code available on{" "}
+        <a href="https://github.com/santosh-marar/santoshmarar.com" className="hover:border-b font-semibold">
+          GitHub
+        </a>.
+      </p>
     </footer>
   );
 };
